6d004aa04caa61b292a0e6b8578044d11e8d735e
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2022-02-28 22:42:21 +0100

    Update packages

--- src/Milochau.Emails/Functions/EmailsFunctions.cs
+++ src/Milochau.Emails/Functions/EmailsFunctions.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using Milochau.Emails.Sdk.Helpers;
 using Microsoft.Azure.Functions.Worker;
 using System.Threading;
+using Azure.Messaging.ServiceBus;
 
 namespace Milochau.Emails.Functions
 {
@@ -27,12 +28,18 @@ namespace Milochau.Emails.Functions
         /// The queue item must be an object of type <see cref="Email"/>
         /// </remarks>
         [Function("SendEmailFromServiceBus")]
-        public async Task SendEmailFromServiceBusAsync([ServiceBusTrigger("emails")] Email email, FunctionContext context)
+        public async Task SendEmailFromServiceBusAsync([ServiceBusTrigger("emails")] ServiceBusMessage message, FunctionContext context)
         {
             var logger = context.GetLogger<EmailsFunctions>();
             logger.LogDebug("Start running SendEmailFromServiceBus Function...");
 
+            throw new Exception();
+
+
+#pragma warning disable CS0162 // Code inaccessible détecté
+            var email = new Email();
             var errors = emailsValidationHelper.ValidateEmail(email);
+#pragma warning restore CS0162 // Code inaccessible détecté
             if (errors != null && errors.Any())
             {
                 var aggregatedErrors = errors.Aggregate((a, b) => a + Environment.NewLine + b);
